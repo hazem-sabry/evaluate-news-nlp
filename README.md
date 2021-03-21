678ce6045a6ff7c326f55acae78763c8ff3113fa
Hazem Sabry <hazem.s@amaz.sa>
2021-03-21 18:28:05 +0200

    init: initialize main project structure

---
+++ README.md
@@ -0,0 +1,5 @@
+![Udacity Banner](https://d20vrrgs8k4bvw.cloudfront.net/images/open-graph/udacity.png)
+
+<h2 align="center">Evaluate news nlp</h2>
+
+
---
+++ src/server/index.js
@@ -0,0 +1,73 @@
+// TODO: Configure the environment variables
+const dotenv = require('dotenv');
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mockAPIResponse = require('./mockAPI.js');
+const axios = require('axios');
+
+dotenv.config();
+
+const API_URL = process.env.API_URL;
+const API_KEY = process.env.API_KEY;
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.use(cors());
+
+// Use json
+app.use(bodyParser.json());
+
+// Use url encoded values
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
+
+app.use(express.static('dist'));
+
+const mockAPIResponse = require('./mockAPI.js');
+
+
+app.get('/', function (req, res) {
+    // res.sendFile('dist/index.html')
+    res.sendFile(path.resolve('src/client/views/index.html'))
+});
+
+// INFO: a route that handling post request for new URL that coming from the frontend
+app.post('/api', async (req, res) => {
+    const { article_url } = req.body
+    const Url = `${API_URL}?key=${API_KEY}&url=${article_url}&lang=en`
+    try {
+        const {
+            data: { score_tag, agreement, subjectivity, confidence, irony }
+        } = await axios(Url)
+        res.send({
+            score_tag: score_tag,
+            agreement: agreement,
+            subjectivity: subjectivity,
+            confidence: confidence,
+            irony: irony
+        })
+    } catch (error) {
+        console.log(error.message)
+    }
+})
+
+app.get('/test', function (req, res) {
+    res.send(mockAPIResponse)
+});
+
+// designates what port the app will listen to for incoming requests
+app.listen(PORT, (error) => {
+    if (error) throw new Error(error)
+    console.log(`Server listening on port ${PORT}!`)
+});
+
+
+module.exports = {
+    app
+}
